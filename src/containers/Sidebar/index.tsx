dc59badaab9c670cf6fe4ba3530a3224b660e8c9
Marcel-ORRJ <marcelo.rrosajr@gmail.com>
2025-07-22 19:30:28 -0300

    Construindo a estrutura

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Marcel-ORRJ.png" />
+    <Titulo>Marcelo R R Junior</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ea fuga vel
+      eligendi quae nulla soluta, nam vero iure nostrum. Ipsam vitae laboriosam
+      fuga quaerat incidunt. Corrupti magnam quam in!
+    </p>
+  </section>
+)
+
+export default Sobre
