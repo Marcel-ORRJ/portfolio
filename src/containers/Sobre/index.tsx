8e327ce30e299fca3b6cc5180fda256617fca2cf
Marcel-ORRJ <marcelo.rrosajr@gmail.com>
2025-07-26 09:04:31 -0300

    Finalizando a seção sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -9,6 +10,10 @@ const Sobre = () => (
       eligendi quae nulla soluta, nam vero iure nostrum. Ipsam vitae laboriosam
       fuga quaerat incidunt. Corrupti magnam quam in!
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=Marcel-ORRJ&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Marcel-ORRJ&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
